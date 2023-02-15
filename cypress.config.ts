cd664abbaeddb9a37171968b6e1ba3136e779385
Risto Virtaharju <rvirtaha@hotmail.com>
2023-02-15 22:35:45 +0000

    cypress runs interactively with docker run -it \                                                                                                                                ✔  28s    -v $PWD:/e2e \   -v /tmp/.X11-unix:/tmp/.X11-unix \   -w /e2e \   -e DISPLAY \   --entrypoint cypress \   cypress/included:12.3.0 open --project .
    however, it won't see localhost:3000
    
    TODO: sort out docker networking

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  e2e: {
+    setupNodeEvents(_on: any, _config: any) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:3000'
+  },
+};
